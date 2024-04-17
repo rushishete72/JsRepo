6552737317bc0c3c23df6c3d79a82ab44cdd0de5
rushishete72 <166739003+rushishete72@users.noreply.github.com>
2024-04-17 14:27:25 +0000

    commit

---
+++ .devcontainer/Basic_of_JS/variables.js
@@ -0,0 +1,15 @@
+const accountId=1212213;
+let accountEmail="Hithesh23@gmail.com";
+var accountPassword="312312";
+accountCity="jaypur";
+var accountState;
+ // accountId=2323; //not allowed
+ accountEmail="Rtfdssd32@gmail.com";
+ accountPassword="23422dsfsd";
+ accountCity="Bengaluru";
+
+/*
+prefer not to use  var
+becuause  ofissues in block scope functional scope 
+*/
+ console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
