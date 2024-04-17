3b4e31536f01598d914370bff2f94cc304a6c8b5
rushishete72 <166739003+rushishete72@users.noreply.github.com>
2024-04-17 15:02:12 +0000

    commit

---
+++ .devcontainer/Basic_of_JS/conversionOperations.js
@@ -0,0 +1,24 @@
+let score=undefined;
+let value=Number(score);
+console.log(typeof score);
+console.log(typeof(value));
+console.log(value);
+ // in numbers
+//"33" =>33
+//"33abc"=> NaN
+//trrue=>1;false=>0;
+
+let isLoggedIn=""
+
+let booleanLogedIn=Boolean(isLoggedIn);
+console.log(typeof booleanLogedIn);
+console.log(booleanLogedIn);
+//in booleans 
+//1=> true; 0=>false
+//""=> false //"fdsf"=>true
+
+//string conversions 
+let stringNumber="234erfg";
+let stringNumberConversions=String(stringNumber);
+console.log(typeof stringNumberConversions);
+console.log(stringNumberConversions);
