a5b855535dde00adec485525a7e5a9a6da16e9a2
rushishete72 <166739003+rushishete72@users.noreply.github.com>
2024-04-17 14:27:00 +0000

    commit

---
+++ .devcontainer/Basic_of_JS/dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3); // we are using nodejs not browser
+console.log(3+3);// code redability should be heigh
+console.log("Hitesh");
+
+let name="Hitesh"
+let age=18;
+let isLoggedIn=False;
+
+//number=>2 to power of53
+//bigint
+//String =>""
+// boolean =>true/false
+//null=>standalone value
+//undefined=>  value is not to bedifine at the time 
+
+// symbol=>unique
+// object
+console.log(typeof null);
